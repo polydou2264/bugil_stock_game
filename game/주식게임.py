6689b870c4d43c625d09aece373bf9ef265c47da
polydou2264 <happykevin13953@gmail.com>
2021-11-05 13:34:03 +0900

    매입까지 구현

--- game/주식게임.py
+++ game/주식게임.py
@@ -1,8 +1,16 @@
 from tkinter import *
 import tkinter
 import tkinter.font
+from tkinter import messagebox
 
 global player
+global player_money_1
+global player_money_2
+global player_money_3
+global player_money_4
+global player_money_5
+
+player_money_1 = [10000]
 
 root = tkinter.Tk()
 root.title("독보기 주식게임")
@@ -21,7 +29,7 @@ def player_1():
     player = "1"
     player_=Tk()
     player_.title("주식 매수")
-    player_.geometry("570x750+670+60")
+    player_.geometry("690x750+610+60")
     player_.iconbitmap('money.ico')
     player_['bg'] = '#404040'
 
@@ -31,9 +39,6 @@ def player_1():
     font_p = tkinter.font.Font(player_, family="Rix열정도", size=23)
     player_num = Label(player_, text="학생1", fg="orange", font=tkinter.font.Font(player_, family="Rix열정도", size=31), bg="#404040")
     player_num.pack()
-    
-    buy_button = Button(player_, text="  매입  ", font = tkinter.font.Font(player_, family="카카오 Bold", size=20)) #매입 버튼
-    buy_button.place(x=100, y=650)
 
     blank_p = Label(player_, text="", width=5, bg="#404040")
     blank_p.pack()
@@ -48,16 +53,16 @@ def player_1():
     #총 12개 종목
     listbox.insert(0,"바이오A")
     listbox.insert(0,"바이오B")
-    listbox.insert(0,"IT_A")
-    listbox.insert(0,"IT_B")
+    listbox.insert(0,"삼성")
+    listbox.insert(0,"애플")
     listbox.insert(0,"화학A")
     listbox.insert(0,"화학B")
     listbox.insert(0,"에너지A")
     listbox.insert(0,"에너지B")
     listbox.insert(0,"제약A")
     listbox.insert(0,"제약B")
-    listbox.insert(0,"식품A")
-    listbox.insert(0,"식품B")
+    listbox.insert(0,"농심")
+    listbox.insert(0,"오뚜기")
     listbox.pack()
 
     scrollbar["command"]=listbox.yview
@@ -66,7 +71,7 @@ def player_1():
     blank_p = Label(player_, text="", height=1, bg="#404040")
     blank_p.pack()
 
-    stock_money = [100,200,300,400,500,600,700,800,900,1000,1100,1200]
+    stock_money = [100,200,300,400,500,600,700,800,900,1000,1100,1200] #주가 가격, 나중에 2차원 배열로 만들어 수정
 
     global pr_money
     global hoho
@@ -78,35 +83,53 @@ def player_1():
         hoho = str(stock_money[menu])
         pr_money = hoho + "₩"
         show_money.configure(text = "1주: "+pr_money)
-        print(pr_money)
     
     show_money = Label(player_, text="1주: "+pr_money, fg="orange", font=font_p, bg="#404040")
     show_money.pack()
 
-    listbox.bind("<<ListboxSelect>>", show_me_the_money)
+    listbox.bind("<<ListboxSelect>>", show_me_the_money) #휠
 
-    blank_p = Label(player_, text="", height=1, bg="#404040")
+    blank_p = Label(player_, text="", height=1, bg="#404040") #공백
     blank_p.pack()
 
-    input_text = Entry(player_, width=5, font=font_p)
+    input_text = Entry(player_, width=5, font=font_p) #주식 개수 입력 받기
     input_text.pack()
 
-    blank_p = Label(player_, text="", height=1, bg="#404040")
-    blank_p.pack()
+    blank_p = Label(player_, text="", height=1, bg="#404040") #공백
+    #blank_p.pack()
 
     global money_2
     money_2= "0₩"
 
-    def buy(): #주식 총 가격 계산 결과 출력
-        #money_2 = int(input_text.get())*int(hoho)
-        print(pr_money)
-        print(int(pr_money.replace("₩","")))
+    def calculate_money(): #주가 가격 얼마인지 보여주는 함수
+        global ca_num
+        menu_ = listbox.curselection()
+        num = int(input_text.get()) #입력받은 값
+        menu = int(menu_[0]) #선택된 항목
+        ca_num = int(stock_money[menu]) * num #총 결과
+        hoho = str(ca_num)
+        money_2 = hoho + "₩"
+        show_money_ls.configure(text = "가격: "+money_2) #출력 값 실시간변경
 
-    btn_choose = Button(player_, text="계산", font=tkinter.font.Font(player_, family="카카오 Bold", size=15), command=buy)
+    btn_choose = Button(player_, text="계산", font=tkinter.font.Font(player_, family="카카오 Bold", size=15), command=calculate_money) #계산 버튼
     btn_choose.pack()
 
-    show_money_ls = Label(player_, text="가격:"+money_2, fg="orange", font=font_p, bg="#404040")
+    show_money_ls = Label(player_, text="가격:"+money_2, fg="white", font=font_p, bg="#404040") #게산된 값 출력
     show_money_ls.pack()
+
+    player_money = Label(player_, text="재산:"+str(player_money_1[0])+"₩", fg="#9DD84B", font=font_p, bg="#404040")
+    player_money.pack()
+
+    def buy_stock():
+        player_money_1[0] = player_money_1[0] - ca_num
+        player_money.configure(text = "재산: "+str(player_money_1[0])+"₩")
+        tkinter.messagebox.showinfo("매입","매입이 정상적으로 완료되었습니다!")
+
+
+
+    buy_button = Button(player_, text="       매입       ", font = tkinter.font.Font(player_, family="카카오 Bold", size=20), command=lambda:[buy_stock()], height=3) #매입 버튼
+    buy_button.place(x=100, y=500)
+
 
 
 
